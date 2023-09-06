45a244e21147719f05c7e7c3ce65f6364a167e96
Röttgers Carsten <carsten.roettgers@gmail.com>
2023-09-06 22:25:09 +0200

    more fixes

--- pkg/plugin/datasource_health_test.go
+++ pkg/plugin/datasource_health_test.go
@@ -14,7 +14,7 @@ func TestCheckHealth(t *testing.T) {
 
 	client := clientMock{statusCode: 200}
 	is := ODataSourceInstance{&client}
-	im.On("Get", mock.Anything).Return(&is, nil)
+	im.On("Get", context.TODO(), mock.Anything).Return(&is, nil)
 
 	// Result
 	result, err := ds.CheckHealth(context.TODO(), &backend.CheckHealthRequest{})
@@ -28,7 +28,7 @@ func TestCheckHealthWithError(t *testing.T) {
 
 	client := clientMock{statusCode: 404}
 	is := ODataSourceInstance{&client}
-	im.On("Get", mock.Anything).Return(&is, nil)
+	im.On("Get", context.TODO(), mock.Anything).Return(&is, nil)
 
 	// Result
 	result, err := ds.CheckHealth(context.TODO(), &backend.CheckHealthRequest{})
